86612ec7b18c7c675970eb8a6b918eb96f53887e
Gokhan-IRTEM <gokirtem@gmail.com>
2022-06-16 23:54:43 +0300

    Initial Commit

---
+++ War Trails/Assets/Scripts/Test1.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+public class Test1 : MonoBehaviour
+{
+    [SerializeField] private float a;
+    // Start is called before the first frame update
+    
+    void Start()
+    {
+         
+    }
+    void Awake()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void FixedUpdate()
+    {
+
+    }
+
+
+
+}
